d9bdfed175861ef0262d39e673af5e342c8251cf
jamiegist <jamiegist@jamies-mbp.mynetworksettings.com>
2024-08-01 20:05:07 -0700

    Basic homepage things.

--- kiwi-records/src/App.js
+++ kiwi-records/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import { NavBar } from './components/navbar';
+import { Home } from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
--- kiwi-records/src/components/navbar.js
+++ kiwi-records/src/components/navbar.js
@@ -7,15 +7,15 @@ export function NavBar() {
                 <a href="index.html">
                     Sky Blue Records
                 </a>
-                <nav class="navbar">
-                    <li class="nav-dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                <nav className="navbar">
+                    <li className="nav-dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                             aria-haspopup="true" aria-expanded="false">
                             Menu
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="index.html">Home</a>
-                            <a class="dropdown-item" href="shop.html">Shop</a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="index.html">Home</a>
+                            <a className="dropdown-item" href="shop.html">Shop</a>
                         </div>
                     </li>
                 </nav>
